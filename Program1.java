8105ccb055d8ab61c1d3bd3776bc1c509de784f4
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 01:25:06 +0200

    How to show outputs

--- Program1.java
+++ Program1.java
@@ -1,8 +1,9 @@
+
 // A program to print your info
 // Understand the program flow
-// keywords in Java
+// keywords in Java - Keywords are reserved words and also in small letters
 // How to create a class, function
-// How to print in Java
+// How to print in Java - use System.out.Println() or print() 
 class Program1{
   public static void main(String[] args){
     System.out.println("Name: Anisul Islam");
---
+++ Program2.java
@@ -0,0 +1,10 @@
+
+// Escape sequence
+
+class Program2{
+  public static void main(String[] args){
+    System.out.println("Name: Anisul Islam");
+    System.out.println("Education: M.Sc. in Software, Web and Cloud");
+    System.out.println("Profession: Full-stack trainer at Integrify, Finland");
+  }
+}
