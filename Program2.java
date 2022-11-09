2e32855977396872263f88ffc9d6e591ed4f50ae
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 01:42:38 +0200

    escape sequences

--- Program2.java
+++ Program2.java
@@ -1,10 +1,11 @@
-
-// Escape sequence
+// Escape sequence - a special character followed by backslash. sometimes it is called as backshalsh character
+// example -> \b, \t, \n, \", \', \\
 
 class Program2{
   public static void main(String[] args){
-    System.out.println("Name: Anisul Islam");
-    System.out.println("Education: M.Sc. in Software, Web and Cloud");
-    System.out.println("Profession: Full-stack trainer at Integrify, Finland");
+    System.out.print("Name: \t\tAnisul Islam\n");
+    System.out.println("Education: \tM.Sc. in Software, Web and Cloud");
+    System.out.println("Profession: \tFull-stack trainer at Integrify, Finland");
+    System.out.println("Passion: \t\"I love coding & teaching\"");
   }
 }
