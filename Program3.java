bb2a6d49b9210f1ff95cbb8a0db681023915423a
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 01:56:47 +0200

    Phases of a java program

---
+++ Program3.java
@@ -0,0 +1,10 @@
+// java program phases
+/*
+ * step 1: Text Editor (.java)
+ * step 2: Compile (javac): comple the .java and create Byte code (.class) which is machine readable
+ * step 3: Load: Class loader load class in memory
+ * step 4: Verify: Class loader verify the class
+ * step 5: Execute: JVM excutes .class file 
+ * 
+ */
+
