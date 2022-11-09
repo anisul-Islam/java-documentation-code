d1df25ca589b472a8ea0794a77b0f7d65f0047bb
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 01:06:13 +0200

    How to show outputs

---
+++ Program1.java
@@ -0,0 +1,12 @@
+// A program to print your info
+// Understand the program flow
+// keywords in Java
+// How to create a class, function
+// How to print in Java
+class Program1{
+  public static void main(String[] args){
+    System.out.println("Name: Anisul Islam");
+    System.out.println("Education: M.Sc. in Software, Web and Cloud");
+    System.out.println("Profession: Full-stack trainer at Integrify, Finland");
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# JAVA Documentation
+
+## 1. Introduction to Java
+
+## 2. Environment setup
+
+## 3. First Java Program
+
+## 4. Escape sequence
