2bf9102dcb2b0119fa35f77d8ca63d60076c1fdc
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 23:24:56 +0200

    assignment 2

---
+++ Assignment2.java
@@ -0,0 +1,9 @@
+/*
+ * Assignment-2
+ * step 1: create a class called Product
+ * step 2: create a main method
+ * step 3: declare variables: id, title, price, description, category
+ * step 4: assign the following data in main method
+101,iphone15,1895 euros,perfect product with best image quality, phone,
+* step 5: print the data
+ */
