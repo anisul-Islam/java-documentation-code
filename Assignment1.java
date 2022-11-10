3e20a58d39c9dc0ced9093d0129497089266b2c8
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-10 23:21:15 +0200

    assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+/*
+ * Assignment-1
+ * create a class called Product
+ * create a main method
+ * print the following data in main method
+
+
+ id: 101,
+title: iphone15,
+price: 1895 euros,
+description: perfect product with best image quality,
+category: phone,
+ */
