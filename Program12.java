c0a4d67e257104fad65234bf936e2d7976ab3666
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-11 01:33:18 +0200

    if,else if and else program

---
+++ Program12.java
@@ -0,0 +1,13 @@
+// if,else if, else program
+public class Program12 {
+  public static void main(String[] args) {
+    int number = 12;
+    if(number>0){
+      System.out.println("Positive");
+    }else if(number<0){
+      System.out.println("Negative");
+    }else{
+      System.out.println("Zero");
+    }
+  }
+}
