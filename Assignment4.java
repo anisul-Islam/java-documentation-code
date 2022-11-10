9722ebf89b871f4c1be1367f05805c3999fa51cb
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-11 00:54:17 +0200

    calculate monthly install amount

---
+++ Assignment4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// Create a program to calculate installment amount for per month
+public class Assignment4 {
+  public static void main(String[] args) {
+
+    try (Scanner input = new Scanner(System.in)) {
+      int phonePrice = 1800; // 1800 euros
+      int numberOfInstallment, installmentPerMonth;
+
+      System.out.print("Number of installments? ");
+      // get number of installments from user
+     
+
+      // calculate  installment amount for per month
+    
+
+        System.out.println("Monthly installment Amount: "+installmentPerMonth + " euros");
+    } 
+
+  }
+}
