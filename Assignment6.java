690b714bf49fb6a2d5c745e1bea0365bdcc21eb6
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-11 01:38:55 +0200

    digit spelling using if,else if and else

---
+++ Assignment6.java
@@ -0,0 +1,11 @@
+/*
+ * Digit spelling program using if,else if, else
+ * step 1: ask for a digit between 0-9
+ * step 2: check the digit and print it by spelling. example if user input is 0 then print Zero
+ * step 3: if the digit is not among 0-9 then print Invalid digit
+ */
+public class Assignment6 {
+  public static void main(String[] args) {
+    
+  }
+}
