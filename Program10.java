c5b1bbcc7f1fa9e3054aedf397c996a8e45e4305
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-11 01:07:58 +0200

    unary operators

---
+++ Program10.java
@@ -0,0 +1,18 @@
+/*
+ * Unary Operator
+ * Unary Plus, minus
+ * increment -> prefix increment, postfix increment
+ * decrement -> prefix decrement, postfix decrement
+ */
+public class Program10 {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(+x);
+    System.out.println(-x);
+    System.out.println(x++);
+    System.out.println(++x);
+    System.out.println(x);
+    System.out.println(x--);
+    System.out.println(--x);
+  }
+}
