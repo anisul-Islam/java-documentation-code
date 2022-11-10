183e6428fc1da53ef518a0eab8582aa2648e702e
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-11 00:06:21 +0200

    assignment operators

---
+++ Program8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Program8 {
+  public static void main(String[] args) {
+   int x = 10;
+   x += 1;
+   System.out.println("x = "+x);
+   x -= 1;
+   System.out.println("x = "+x);
+   x *= 1;
+   System.out.println("x = "+x);
+   x /= 1;
+   System.out.println("x = "+x);
+   x %= 1;
+   System.out.println("x = "+x);
+  }
+}
