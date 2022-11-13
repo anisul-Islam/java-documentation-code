d14988063d2302718b4fcd3d1f3fdbe15a2684f5
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-13 02:58:51 +0200

    conditional operator

---
+++ Program16.java
@@ -0,0 +1,8 @@
+// ternary/conditional operator exp1 ? exp2 : exp3
+public class Program16 {
+  public static void main(String[] args) {
+    int number = 20;
+    String result = number%2==0 ? "even" : "odd";
+    System.out.println(result);
+  }
+}
