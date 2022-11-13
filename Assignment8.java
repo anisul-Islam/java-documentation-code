a4adfe0e9cfaf6793307eae2691ef115266453eb
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-13 02:35:33 +0200

    logical and assignment

---
+++ Assignment8.java
@@ -0,0 +1,11 @@
+// Logical and assignment
+// Check eligible candidate
+// Step 1: Ask the candidate have you completed your masters? y/n 
+// Step 2: Ask the candidate are you fulent in English? y/n 
+// Step 1: if the candidate has passed masters and also have fluent english skill then print you are eligible to for the job interview
+// Step 2: else print Sorry. you are not eligible to for the job interview
+public class Assignment8 {
+  public static void main(String[] args) {
+    
+  }
+}
