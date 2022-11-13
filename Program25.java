1a5280f8c9d49d3d1019593ce3690f792896d198
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-13 04:55:15 +0200

    prime numbers from m-n

---
+++ Program25.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Program25
+ {
+   public static void main(String args[])
+   { 
+     try (Scanner input = new Scanner(System.in)) {
+        int m,n;
+        int count =0;
+        int totalprime=0; 
+        System.out.print("Enter initial number:");
+        m = input.nextInt();
+        System.out.print("Enter last number: ");
+        n = input.nextInt();
+      
+        for(int i=m;i<=n;i++)
+        {
+          if(i==0 || i==1){
+            continue;
+          }else{
+            for(int j=2;j<i;j++)
+            {        
+              if(i%j==0)
+              {
+                count++;
+                break;
+              }
+            } 
+            if(count==0){
+              System.out.println(i);  
+              totalprime++;           
+            }
+            count=0;
+          }          
+        }
+        System.out.println("total prime:"+totalprime);
+      }    
+    }
+  }
