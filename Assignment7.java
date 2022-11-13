d1f08f7c80124fb9dad4a2043e7065e84384d478
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-13 02:28:46 +0200

    logical or assignment

---
+++ Assignment7.java
@@ -0,0 +1,10 @@
+// Logical or assignment
+// step 1: Print "Do you love java? "
+// step 2: take user input y / Y / n / N
+// step 3: if user input y / Y then print you are a java lover
+// step 4: if user input n / N then print you are not a java lover
+public class Assignment7 {
+  public static void main(String[] args) {
+    
+  }
+}
