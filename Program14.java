9a6e50040991a870bafcdff47a4c204b09b14713
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-13 02:12:58 +0200

    small/capital letter program using logical and

---
+++ Program14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// small /capital letter
+public class Program14 {
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter a letter: ");
+      char ch = input.next().charAt(0);
+
+      if(ch >='a' && ch <= 'z'){
+        System.out.println("Small letter");
+      }else if(ch >='A' && ch <= 'Z'){
+        System.out.println("Capital letter");
+      }else{
+         System.out.println("Not a letter");
+      }
+
+    }
+    
+  }
+}
