0c1f269de50580bfbbb7b6c34bc4b56eac6d593a
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-15 11:47:27 +0200

    palindrome numbers

---
+++ Program29.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// SUM OF digits
+// Reverse
+// Palindrome
+// Armstorng
+public class Program29 {
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter any postive integer: ");
+      int number = input.nextInt();
+      int temp = number;
+      int sum = 0;
+      while(temp!=0){
+          int r = temp % 10;
+          sum = sum *10 + r;
+          temp = temp / 10;
+      }
+     if(number == sum){
+      System.out.println("Palindrome number");
+     }
+     else{
+      System.out.println("Not a Palindrome number");
+     }
+    }
+  }
+}
