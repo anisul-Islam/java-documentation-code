66456de6e484410d9dfe9c5e666f1fc3917a7dd3
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-15 08:00:34 +0200

    fibonacci assignment

---
+++ Assignment13.java
@@ -0,0 +1,14 @@
+// find nth fibonacci number
+import java.util.Scanner;
+public class Assignment13 {
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in)) {
+
+      System.out.print("which fibonacci number you want to see? ");
+      int n = input.nextInt();
+
+     
+    }
+  }
+}
+ 
