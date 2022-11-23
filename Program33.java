d2dbf877e0bff4c6fd70281484717e39b643cdbb
anisul-islam <anisul2010s@yahoo.co.uk>
2022-11-24 01:33:52 +0200

    enhanced for loop

--- Program33.java
+++ Program33.java
@@ -1,11 +1,16 @@
+// for each or enhanched for loop
 import java.util.Scanner;
-
 
 public class Program33 {
   public static void main(String[] args) {
 
        try (Scanner input = new Scanner(System.in)) {
         
+        String[] countries = {"Bangladesh", "Pakistan", "England", "India"};
+
+        for (String country : countries) {
+          System.out.println(country);
+        }
          
       }
   }
