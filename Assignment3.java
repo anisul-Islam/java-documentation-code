e36f99670dbfb85dfc34471f7a9bb32e697b56c2
Shadat Hossan <119412677+shadat-hossan@users.noreply.github.com>
2024-03-08 09:21:27 -0800

    Assignment3 complete .java
    Assignment 3 successfully completed

--- Assignment3.java
+++ Assignment3.java
@@ -6,3 +6,33 @@
  * step 4: get user input for each variables
 * step 5: print the variables
  */
+
+
+import java.util.*;
+
+public class Product {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Assign Product ID: ");
+        int id = sc.nextInt();
+        
+        System.out.print("Assign Product Title: ");
+        String title = sc.nextLine();
+        
+        System.out.print("Assign Product Price: ");
+        double price = sc.nextDouble(); 
+        
+        System.out.print("Assign Product Description: ");
+        String description = sc.nextLine(); 
+        
+        System.out.print("Assign Product Category: ");
+        String category = sc.nextLine(); 
+        
+        System.out.println("id: " + id);
+        System.out.println("title: " + title);
+        System.out.println("price: " + price + " euros");
+        System.out.println("description: " + description);
+        System.out.println("category: " + category);
+    }
+}
